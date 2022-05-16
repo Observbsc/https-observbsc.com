2bf52ef7cd04cac082e1f3fe7c827ea6aad9afbf
gingerzadddy <hello@observbsc.com>
2022-05-16 14:18:35 -0400

    Update truncateHash.ts

--- src/utils/truncateHash.ts
+++ src/utils/truncateHash.ts
@@ -1,10 +1,10 @@
 /**
  * Truncate a transaction or address hash
  */
-const truncateHash = (address: string, startLength = 50) => {
+ const truncateHash = (address: string, startLength = 50, endLength = 0) => {
   if (!address) return ''
 
-  return `${address.substring(50, startLength)}`
+  return `${address.substring(0, startLength)}${address.substring(address.length - endLength)}`
 }
 
 export default truncateHash
