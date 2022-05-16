b11d620ec0025bcb58a673301065bf7205e4611d
gingerzadddy <hello@observbsc.com>
2022-05-16 16:15:57 -0400

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -54,16 +54,16 @@ function MyApp(props: AppProps) {
         />
         <meta
           name="description"
-          content="Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE."
+          content="Observ Ecosystem on Binance Smart Chain"
         />
         <meta name="theme-color" content="#1FC7D4" />
-        <meta name="twitter:image" content="https://pancakeswap.finance/images/hero.png" />
+        <meta name="twitter:image" content="https://github.com/Observbsc/Logos/blob/main/logo500x500.png?raw=true" />
         <meta
           name="twitter:description"
-          content="The most popular AMM on BSC! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust."
+          content="Observ Ecosystem on Binance Smart Chain"
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)" />
+        <meta name="twitter:title" content="Observ Ecosystem on Binance Smart Chain" />
         <title>Observ</title>
       </Head>
       <Providers store={store}>
