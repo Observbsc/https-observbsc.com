6f745689fdb2e69b406cf79d8676ef85a14e9697
gingerzadddy <hello@observbsc.com>
2022-05-16 15:14:49 -0400

    Update Phishing

--- src/components/PhishingWarningBanner/index.tsx
+++ src/components/PhishingWarningBanner/index.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components'
-import { Text, Flex, Box, CloseIcon, IconButton, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { Text, Flex, Box, useMatchBreakpoints } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
-import { usePhishingBannerManager } from 'state/user/hooks'
 
 const Container = styled(Flex)`
-  overflow: hidden;
   height: 100%;
-  padding: 12px;
+  padding: 0px;
   align-items: center;
   background: linear-gradient(0deg, rgba(39, 38, 44, 0.4), rgba(39, 38, 44, 0.4)),
     linear-gradient(180deg, #06051b 0%, #696969 100%);
   ${({ theme }) => theme.mediaQueries.md} {
     padding: 0px;
+    align-items: center;
+    justify-content: center;
     background: linear-gradient(180deg, #06051b 0%, #696969 100%);
   }
 `
@@ -26,22 +26,22 @@ const InnerContainer = styled(Flex)`
 const SpeechBubble = styled.div`
   background: rgba(39, 38, 44, 0.4);
   border-radius: 16px;
-  padding: 8px;
+  padding: 0px;
   width: 60%;
   height: 80%;
   display: flex;
+  justify-content: center;
   align-items: center;
   flex-wrap: wrap;
 
   & ${Text} {
     flex-shrink: 0;
-    margin-right: 4px;
+    margin-right: 0px;
   }
 `
 
 const PhishingWarningBanner: React.FC = () => {
   const { t } = useTranslation()
-  const [, hideBanner] = usePhishingBannerManager()
   const { isMobile, isMd } = useMatchBreakpoints()
   const warningText = t("0x5e56394422782690653733DA4829c056B900BD18")
   const warningTextAsParts = warningText.split(/(https:\/\/pancakeswap.finance)/g)
@@ -56,6 +56,7 @@ const PhishingWarningBanner: React.FC = () => {
           key={i}
           small
           as="span"
+          font-sezi="12px"
           bold={text === '0x5e56394422782690653733DA4829c056B900BD18'}
           color={text === '0x5e56394422782690653733DA4829c056B900BD18' ? '#FFFFFF' : '#BDC2C4'}
         >
@@ -69,9 +70,6 @@ const PhishingWarningBanner: React.FC = () => {
       {isMobile || isMd ? (
         <>
           <Box>{warningTextComponent}</Box>
-          <IconButton onClick={hideBanner} variant="text">
-            <CloseIcon color="#FFFFFF" />
-          </IconButton>
         </>
       ) : (
         <>
